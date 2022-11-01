5a6587a12e8a431994b5200e29513cf6637aac4e
Zeynep Sumeyye Gungor <gungorsumeyye3520@gmail.com>
2022-11-02 00:51:21 +0300

    02/11/22

---
+++ src/test/java/AA_Ozet.java
@@ -0,0 +1,18 @@
+public class AA_Ozet {
+    /*
+    Soap=finansal ve ödeme noktaları ile alakalı alanlarda kullanılır, daha güvenilirdir.
+    Kendine özgü paketleme yöntemleri oldugundan dolayı hızlıdır, ön bellegi okuyamaz.
+
+    PUT : Bir kaynakta verinin tamamının değiştirilmesi için kullanılan methoddur.
+    Var olan kayıt üzerinde değişiklik yaparken mevcut verinin tamamını sileceğinden,
+    sadece bir veriyi değiştirecek olsak da tüm body'yi göndermemiz gerekir.
+
+    PATCH : Belirli bir kaynaktaki verilerin bir kısmının değiştirilmesi için kullanılan methoddur.
+    Güncellediği veriyi baştan oluşturmaz. Yalnızca değişiklik yapmak istenilen yerdeki değeri değiştirir.
+
+
+
+
+
+     */
+}
