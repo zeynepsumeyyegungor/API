4e5ca1bcec33e4bfbb6e6903ef579691bf82c7cb
Zeynep Sumeyye Gungor <gungorsumeyye3520@gmail.com>
2022-11-08 02:15:46 +0300

    08/11/2022

--- src/test/java/utils/ObjectMapperUtils.java
+++ src/test/java/utils/ObjectMapperUtils.java
@@ -3,8 +3,10 @@ package utils;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ObjectMapperUtils {
+
     //new ObjectMapper().readValue(jsonInString, HashMap .class);
 
     private static ObjectMapper mapper;
@@ -13,16 +15,16 @@ public class ObjectMapperUtils {
         mapper = new ObjectMapper();
     }
 
-    public static <T> T convertJsonToJava(String json, Class<T> cls) { //Generic Method
+    public static <T> T convertJsonToJava(String json, Class<T> cls){//Generic Method
 
         T javaResult = null;
 
         try {
-            javaResult = mapper.readValue(json, cls);
+            javaResult = mapper.readValue(json,cls);
         } catch (IOException e) {
             throw new RuntimeException(e);
-
         }
+
         return javaResult;
     }
 }
